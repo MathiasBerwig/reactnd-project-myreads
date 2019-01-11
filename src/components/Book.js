91e6f548c6c14e43cc3633ba9b684408797f02a9
Mathias Berwig <mathiasberwig@gmail.com>
2019-01-11 12:27:33 -0200

    Refactor Book to PureComponent

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import BookShelfChanger from "./BookShelfChanger";
 
-export default class Book extends Component {
+export default class Book extends PureComponent {
   static propTypes = {
     value: PropTypes.object.isRequired,
     books: PropTypes.array.isRequired,
